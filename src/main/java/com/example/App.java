5a338fe41818c75461b257d4bd55a1a9d4ce1df0
alessio <pan.alessio@itismeucci.com>
2023-10-11 11:16:48 +0200

    secondo commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    
+        try{
+
+            System.out.println("server in esecuzione");
+            ServerSocket Server = new ServerSocket(3344);
+            Socket s = Server.accept();
+            
+            BufferedReader in = new BufferedReader (new InputStreamReader (s.getInputStream()));
+            DataOutputStream out  = new DataOutputStream(s.getOutputStream());
+
+            String receive = in.readLine();
+            String send = receive.toUpperCase();
+            out.writeBytes( send + "\n");
+
+
+        }
+        catch(Exception e){
+
+            System.out.println(e.getMessage());
+            System.out.println(e.getMessage());
+            System.exit(1);
+
+
+        }
+        
+
+
+
+
+
     }
 }
